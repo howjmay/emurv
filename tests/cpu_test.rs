32acf3965e6f9143211651b9b0b92f6a05452ebd
Yang Hau <yuanyanghau@gmail.com>
2024-02-13 21:17:03 +0800

    test: Fix u type tests

--- tests/cpu_test.rs
+++ tests/cpu_test.rs
@@ -10,12 +10,12 @@ mod tests {
         let mut cpu_test = cpu::CPU::new();
 
         // lui x5, 4
-        let instr: u32 = helper::set_u_type_instruction(4 << 12, 5, LUI as u8);
+        let instr: u32 = helper::set_u_type_instruction(4, 5, LUI as u8);
         cpu::exec_lui(&mut cpu_test, instr);
         assert_eq!(cpu_test.xregs.regs[5], 4 << 12);
 
         // lui x5, -4
-        let instr: u32 = helper::set_u_type_instruction(-(4 << 12), 5, LUI as u8);
+        let instr: u32 = helper::set_u_type_instruction(-4, 5, LUI as u8);
         cpu::exec_lui(&mut cpu_test, instr);
         assert_eq!(cpu_test.xregs.regs[5], -(4 << 12) as u64);
     }
@@ -25,12 +25,12 @@ mod tests {
 
         let ori_pc = cpu_test.pc;
         // auipc x5, 4
-        let instr: u32 = helper::set_u_type_instruction(4 << 12, 5, AUIPC as u8);
+        let instr: u32 = helper::set_u_type_instruction(4, 5, AUIPC as u8);
         cpu::exec_auipc(&mut cpu_test, instr);
         assert_eq!(cpu_test.xregs.regs[5], ori_pc + (4 << 12));
 
         // auipc x5, -4
-        let instr: u32 = helper::set_u_type_instruction(-(4 << 12), 5, AUIPC as u8);
+        let instr: u32 = helper::set_u_type_instruction(-4, 5, AUIPC as u8);
         cpu::exec_auipc(&mut cpu_test, instr);
         assert_eq!(cpu_test.xregs.regs[5], (ori_pc as i64 + (-4 << 12)) as u64);
     }
